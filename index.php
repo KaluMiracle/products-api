63bfde35e2c4a79cdbaf3cff462c2a491f7a26b2
KaluMiracle <60604821+KaluMiracle@users.noreply.github.com>
2022-03-22 08:29:28 +0100

    e

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
 
-require __DIR__ . '\vendor\autoload.php';
-
-use Source\Config\Database;
-
-
-
 echo json_encode('Hello to products API. Oops incorrect endpoint');
 
-$ff = new Database();
